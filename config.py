5f5fd700aed4f36ae32c8109eb2f0455ca2788de
kane5 <Nguyenkhactam5@gmail.com>
2025-05-15 09:09:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ API_ID = os.getenv("API_ID", "23947736")
 API_HASH = os.getenv("API_HASH", "0dc69538a19d6f4644343a20a7eb1ca9")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7587414782:AAFKrNmxNl6t4juM2R2zWAvxC8bbew_n-cE")
 MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://nguyenkhactam5:<db_password>@kanereaction.zhqqd.mongodb.net/?retryWrites=true&w=majority&appName=kanereaction")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "6705263463,6383614933").split())) # list seperated via space
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "6705263463,6383614933").split(','))) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "download_content")
 STRING = os.getenv("STRING", None) # optional
 LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002627666364")) # optional with -100
